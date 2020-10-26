d8547ded74a255de36da8118fd13c185e4793d07
Estevanus <oscartoreh@gmail.com>
2020-10-26 19:08:12 +0800

    UpBGE Files

---
+++ var.py
@@ -0,0 +1,21 @@
+import bge
+lok = bge.logic.expandPath("//")
+
+isStart = False
+w = None
+handle = None
+totalFrames = 0
+frameRate = 0
+totalTime = 0
+
+lastFrame = 0
+lastN = 0
+
+
+startYPos = 0
+
+isPlus = 1
+
+frames = []
+
+
